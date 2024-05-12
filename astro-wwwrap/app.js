ddeb553f71464de3a60a5eafa3c335102eb48485
verdulife <verdu@live.com>
2024-05-12 10:01:22 +0200

    todo fix iframe src

--- astro-wwwrap/app.js
+++ astro-wwwrap/app.js
@@ -2,22 +2,23 @@ import { defineToolbarApp } from "astro/toolbar";
 
 export default defineToolbarApp({
   init(canvas, app) {
-    const { host } = window.location;
-    const frame = document.createElement("iframe");
-
-    frame.src = `/wwwrap/${host}`;
-    canvas.appendChild(frame);
-
-    frame.style.cssText = /* css */`
-      position: fixed;
-      width: 100vw;
-      height: 100vh;
-      border: none;
-      z-index: 9999; 
-    `;
-
     app.onToggled(({ state }) => {
       if (!state) return;
+
+      const { host, pathname } = window.location;
+      const frame = document.createElement("iframe");
+      const URL = "/wwwrap/" + (`${host}${pathname}`).replaceAll("/", "_");
+
+      frame.src = URL;
+      canvas.appendChild(frame);
+
+      frame.style.cssText = /* css */`
+        position: fixed;
+        width: 100vw;
+        height: 100vh;
+        border: none;
+        z-index: 9999; 
+      `;
 
       const inner = frame.contentDocument.documentElement.querySelector("astro-dev-toolbar");
 
